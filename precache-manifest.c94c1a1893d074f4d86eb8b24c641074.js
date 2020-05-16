99453985baabbc1cb1af545d818785bfe90b63aa
Robin Steeman <Robin_steeman@hotmail.com>
2020-05-16 10:51:22 +0900

    Updates

---
+++ precache-manifest.c94c1a1893d074f4d86eb8b24c641074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752fafb9b349599124f9b7c6eb383c7e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4db7b7801038112d2408",
+    "url": "/monsters-rolodex/static/css/main.5a4367a3.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db7b7801038112d2408",
+    "url": "/monsters-rolodex/static/js/main.f5c447bf.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
